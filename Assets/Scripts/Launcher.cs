106f3967982955027d3cb1c0740e79d4b67679c5
Fernando <fmelara750@gmail.com>
2024-04-28 03:09:46 -0600

    Guia3
    Contenido desarrollado de la guia 3

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -1,10 +1,11 @@
-using System;
-using System.Collections;
+
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
-using UnityEditor.VersionControl;
+
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
@@ -12,6 +13,17 @@ public class Launcher : MonoBehaviourPunCallbacks
    [SerializeField] TMP_InputField roomNameInputField;
    [SerializeField] TMP_Text roomName;
    [SerializeField] TMP_Text ErrorMessage;
+   [SerializeField] Transform roomListContent;
+   [SerializeField] GameObject roomItemPrefab;
+   public static Launcher Instance;
+   [SerializeField] Transform PlayerListContent;
+   [SerializeField] GameObject PlayerItemPrefab;
+
+   private void Awake()
+   {
+      Instance = this;
+   }
+
    private void Start()
    {
       Debug.Log("Conectando");
@@ -29,7 +41,7 @@ public class Launcher : MonoBehaviourPunCallbacks
    {
       Debug.Log("Conectado al lobby");
       MenuManager.Instance.OpenMenuName("Home");
-      
+      PhotonNetwork.NickName = "player" + Random.Range(0, 1000).ToString("0000");
    }
 
    public void CreateRoom()
@@ -48,6 +60,17 @@ public class Launcher : MonoBehaviourPunCallbacks
    {
       MenuManager.Instance.OpenMenuName("Room");
       roomName.text = PhotonNetwork.CurrentRoom.Name;
+      foreach (Transform playerT in PlayerListContent)
+      {
+         Destroy(playerT.gameObject);
+      }
+
+      Player[] players = PhotonNetwork.PlayerList;
+
+      for (int i = 0; i < players.Count(); i++)
+      {
+         Instantiate(PlayerItemPrefab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+      }
    }
 
    public override void OnCreateRoomFailed(short returnCode, string message)
@@ -56,6 +79,11 @@ public class Launcher : MonoBehaviourPunCallbacks
       MenuManager.Instance.OpenMenuName("Error");
    }
 
+   public void JoinRoom(RoomInfo _info)
+   {
+      PhotonNetwork.JoinRoom(_info.Name);
+      MenuManager.Instance.OpenMenuName("Loading");
+   }
    public void LeaveRoom()
    {
       PhotonNetwork.LeaveRoom();
@@ -65,5 +93,26 @@ public class Launcher : MonoBehaviourPunCallbacks
    public override void OnLeftRoom()
    {
       MenuManager.Instance.OpenMenuName("Home");
+   }
+
+   public override void OnRoomListUpdate(List<RoomInfo> roomList)
+   {
+      foreach (Transform transfo in roomListContent)
+      {
+         Destroy(transfo.gameObject);
+      }
+
+      for (int i = 0; i < roomList.Count; i++)
+      {
+         if (roomList[i].RemovedFromList)
+         {continue; }
+         
+         Instantiate(roomItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+      }
+   }
+
+   public override void OnPlayerEnteredRoom(Player newPlayer)
+   {
+      Instantiate(PlayerItemPrefab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
    }
 }
