389b768fabbc96175197d7f372cef1c038ab9a6f
Fernando <fmelara750@gmail.com>
2024-04-28 01:30:29 -0600

    Guia2
    Se desarrolla el contenido de la guia 2

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,9 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
+using UnityEditor.VersionControl;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+
+   [SerializeField] TMP_InputField roomNameInputField;
+   [SerializeField] TMP_Text roomName;
+   [SerializeField] TMP_Text ErrorMessage;
    private void Start()
    {
       Debug.Log("Conectando");
@@ -22,6 +28,42 @@ public class Launcher : MonoBehaviourPunCallbacks
    public override void OnJoinedLobby()
    {
       Debug.Log("Conectado al lobby");
+      MenuManager.Instance.OpenMenuName("Home");
+      
+   }
+
+   public void CreateRoom()
+   {
+      if (string.IsNullOrEmpty(roomNameInputField.text))
+      {
+         return;
+      }
+
+      PhotonNetwork.CreateRoom(roomNameInputField.text);
+      
+      MenuManager.Instance.OpenMenuName("Loading");
+   }
+
+   public override void OnJoinedRoom()
+   {
+      MenuManager.Instance.OpenMenuName("Room");
+      roomName.text = PhotonNetwork.CurrentRoom.Name;
+   }
+
+   public override void OnCreateRoomFailed(short returnCode, string message)
+   {
+      ErrorMessage.text = "Error al crear la sala" + message;
+      MenuManager.Instance.OpenMenuName("Error");
+   }
+
+   public void LeaveRoom()
+   {
+      PhotonNetwork.LeaveRoom();
+      MenuManager.Instance.OpenMenuName("Loading");
+   }
+
+   public override void OnLeftRoom()
+   {
       MenuManager.Instance.OpenMenuName("Home");
    }
 }
